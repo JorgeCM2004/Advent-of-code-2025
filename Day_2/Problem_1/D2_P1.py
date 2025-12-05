e741bbc40201fc316c4b9b2ff8fe78a529fdeeea
JorgeCM <harryj3699@gmail.com>
2025-12-05 12:54:18 +0100

    Day 2 Problem 2 resolved

---
+++ Day_2/Problem_1/D2_P1.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+def invalid_until(number):
+	i = 1
+	invalids = []
+	while int(str(i) + str(i)) <= number:
+		invalids.append(int(str(i) + str(i)))
+		i += 1
+	return invalids
+
+def main():
+	counter = 0
+	with open(Path(__file__).parent.parent / "input.txt", "r") as file:
+		file = file.readline()
+		file = [x.strip() for x in file.split(",")]
+		data = []
+		for i in file:
+			x, y = i.split("-")
+			data.append((int(x), int(y)))
+
+	for x, y in data:
+		invalids = invalid_until(y)
+		invalids = [inv for inv in invalids if inv >= x]
+		counter += sum(invalids)
+	print(counter)
+
+
+if __name__ == "__main__":
+	main()
